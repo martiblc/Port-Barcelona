0a77b785a828fe8d18ca071397386c68ba9b61b3
martiblc <62562807+martiblc@users.noreply.github.com>
2022-08-28 20:10:40 +0200

    Create buques_prev.R

---
+++ buques_prev.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(lubridate)
+
+# llegadas
+prev_llegadas_BCN <- read_csv("https://opendata.portdebarcelona.cat/dataset/342fe09b-017b-4019-a743-ee773f09befd/resource/72f0fc9e-b4b4-4a61-a0fb-e7b65b601b4d/download/arribadesavui.csv")
+write_csv(prev_llegadas_BCN, paste0("data/llegadas_previstas/prev_llegadas_BCN_", today(), ".csv"))
+
+#Salidas
+prev_salidas_BCN <- read_csv("https://opendata.portdebarcelona.cat/dataset/342fe09b-017b-4019-a743-ee773f09befd/resource/4bf1ccd0-5132-4d54-81d4-1ab72d5542e9/download/sortidesavui.csv")
+write_csv(prev_salidas_BCN, paste0("data/salidas_previstas/prev_salidas_BCN_", today(), ".csv"))
